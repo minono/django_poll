6ec1d2315b1e403d7cc97924331cb84f9d90adb2
minono <minori.nakamura1209@gmail.com>
2017-07-26 18:28:08 +0900

    Finish tutorial 2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig', # 追加した分
 ]
 
 MIDDLEWARE_CLASSES = [
